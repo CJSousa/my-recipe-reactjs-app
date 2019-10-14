200025d98cecf2b60ba6a90224a4e5ec29f6a647
CJSousa <51022263+CJSousa@users.noreply.github.com>
2019-10-14 19:08:47 +0100

    Changing default search

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState('');
-  const [query, setQuery] = useState('chicken');
+  const [query, setQuery] = useState('rice');
 
   useEffect(() => {
     getRecipes();
